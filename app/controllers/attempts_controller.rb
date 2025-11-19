a189697741f10938f8634da1d609140ce6af54a2
Hasan7e <hasan7e@gmail.com>
2025-11-19 00:39:51 +0000

    added new styling cards for front page and upgrading the choice adding system for questions

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,8 +1,12 @@
 class AttemptsController < ApplicationController
   def show
-    @attempt = Attempt.find(params[:id])
+    @attempt = Attempt.includes(answers: { question: :choices }).find(params[:id])
     @quiz    = @attempt.quiz
     @score   = @attempt.score
     @total   = @quiz.questions.count
+
+    # build a quick lookup hash: question_id => answer
+    @answers_by_question_id = @attempt.answers.index_by(&:question_id)
   end
 end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   has_many :choices, dependent: :destroy
+
+  def label_for_choice_form
+    "Q#{id} | Quiz #{quiz_id} - #{quiz.title} | #{text}"
+  end
 end
+
+
