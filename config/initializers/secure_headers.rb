06685ec4bb9bbbd1a453e4952f08ab0ac84116c3
Hasan7e <hasan7e@gmail.com>
2025-12-01 01:20:42 +0000

    fixed a bug that was breaking the logout due to secure_headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -10,17 +10,35 @@ SecureHeaders::Configuration.default do |config|
   config.x_frame_options = "DENY"
   config.x_content_type_options = "nosniff"
   config.x_xss_protection = "1; mode=block"
-  config.x_permitted_cross_domain_policies = "none"
-
   config.referrer_policy = "strict-origin-when-cross-origin"
 
-  # Content Security Policy
   config.csp = {
-  default_src: %w['self'],
-  script_src: %w['self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com],
-  style_src: %w['self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com],
-  font_src: %w['self' data: https://fonts.gstatic.com],
-  img_src: %w['self' data:],
+    default_src: %w['self'],
+    script_src: %w[
+      'self'
+      'unsafe-inline'
+      https://cdn.jsdelivr.net
+      https://cdnjs.cloudflare.com
+      https://cdn.jsdelivr.net/npm/es-module-shims
+    ],
+    style_src: %w[
+      'self'
+      'unsafe-inline'
+      https://cdn.jsdelivr.net
+      https://fonts.googleapis.com
+    ],
+    font_src: %w[
+      'self'
+      data:
+      https://fonts.gstatic.com
+    ],
+    img_src: %w[
+      'self'
+      data:
+    ],
+    connect_src: %w[
+      'self'
+      https://cdn.jsdelivr.net
+    ]
   }
-
 end
